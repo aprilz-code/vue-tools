b597488d3ea1107ada3a2829b621e306a08a52e8
white <liushaohui777@163.com>
2023-01-19 12:44:26 +0800

    图片支持下载

--- src/pages/tools/tbShow.vue
+++ src/pages/tools/tbShow.vue
@@ -4,13 +4,18 @@
             <el-row :gutter="10">
                 <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
                     <div style="">
-                        <div class="desc" >
-                            <img :src="picUrl" :alt="title"  >
+                        <div class="desc">
+                            <img :src="picUrl" :alt="title">
                         </div>
-                        <div class="desc title">{{title}}</div>
+                        <div class="desc title">{{ title }}</div>
                     </div>
                     <div style="margin:0 auto;width:80%;">
-                        <div class="desc"><el-button type="warning" icon="el-icon-picture" @click="getData">下一张</el-button></div>
+                        <div class="desc">
+                            <el-button type="warning" icon="el-icon-picture" @click="getData">下一张</el-button>
+                        </div>
+                        <div class="desc">
+                            <el-button type="warning" icon="el-icon-download" @click="downloadImg">图片下载</el-button>
+                        </div>
                     </div>
                 </el-col>
             </el-row>
@@ -23,25 +28,46 @@
 
     export default {
         name: "TBShow",
-        data(){
-            return{
-                title:'',
-                picUrl:''
+        data() {
+            return {
+                title: '',
+                picUrl: ''
             }
         },
         created() {
             this.getData()
         },
         methods: {
-            getData(){
-                getTbShow().then(res=> {
-                    if(!res && res.status !== 200){
+            getData() {
+                getTbShow().then(res => {
+                    if (!res && res.status !== 200) {
                         this.$message.error(JSON.stringify(res))
                     }
-                    this.title= res.data.title
-                    this.picUrl= res.data.pic
+                    this.title = res.data.title
+                    this.picUrl = res.data.pic
 
                 })
+            },
+            downloadImg() {
+                var image = new Image();
+                // 解决跨域 Canvas 污染问题
+                image.setAttribute("crossOrigin", "anonymous");
+                let _that = this
+                image.onload = function () {
+                    var canvas = document.createElement("canvas");
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    var context = canvas.getContext("2d");
+                    context.drawImage(image, 0, 0, image.width, image.height);
+                    var url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+
+                    var a = document.createElement("a"); // 生成一个a元素
+                    var event = new MouseEvent("click"); // 创建一个单击事件
+                    a.download = _that.title || "photo"; // 设置图片名称
+                    a.href = url; // 将生成的URL设置为a.href属性
+                    a.dispatchEvent(event); // 触发a的单击事件
+                };
+                image.src = this.picUrl;
             }
         }
 
@@ -49,35 +75,39 @@
 </script>
 
 <style scoped>
-    .p{
+    .p {
         background-color: black;
-        background-size:cover;
+        background-size: cover;
         width: 100%;
     }
-    .desc{
+
+    .desc {
         text-align: center;
         margin-top: 1vh;
     }
-    .desc.title{
+
+    .desc.title {
         color: #FFF;
         margin-top: 1em;
         margin-bottom: 8px;
         font-weight: 700;
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
-    img{
+
+    img {
         text-align: center;
-        overflow:hidden;
-        max-width:100%;
+        overflow: hidden;
+        max-width: 100%;
         max-height: 800px;
         box-shadow: 4px 4px 4px rgba(177, 174, 172, 0.6);
         border-radius: 4px;
     }
-    button{
+
+    button {
         color: #000;
         margin-top: 6px;
         background-color: #F90;
         font-size: 17px;
-        font-family:'Times New Roman', Times, serif
+        font-family: 'Times New Roman', Times, serif
     }
 </style>
